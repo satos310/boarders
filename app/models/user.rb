afdcb060612c6177fca1f58b0fa8056c86c53894
satos310 <119788681+satos310@users.noreply.github.com>
2023-05-17 16:17:04 +0000

    [add ActiveStrage]boarders

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,16 @@ class User < ApplicationRecord
   has_many :pickups, dependent: :destroy
   has_many :notifications, dependent: :destroy
 
+  has_one_attached :user_image
+
+  def get_item_image(width, height)
+    unless item_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      item_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    item_image.variant(resize_to_limit: [width, height]).processed
+  end
+
   # ユーザー名が3文字未満の場合はエラーを出すようにしたいが
   # TerminalでArugument Error（引数エラー）が発生する　↓
 
