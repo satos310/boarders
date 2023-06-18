092648104c27d8517178cc6ee7ee80cb664ec4f5
satos310 <119788681+satos310@users.noreply.github.com>
2023-06-18 08:39:19 +0000

    [fix-layout admin/user/index]boarders

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,8 +1,11 @@
 class Admin::UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+    @reviews = @user.reviews
   end
 
   def edit
