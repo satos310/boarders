12c7f87a2c2f1dbd5cf71ed7476528c80b254304
satos310 <119788681+satos310@users.noreply.github.com>
2023-06-25 00:22:59 +0000

    [fix relationships_default]boarders

--- db/migrate/20230515133229_create_relationships.rb
+++ db/migrate/20230515133229_create_relationships.rb
@@ -1,8 +1,8 @@
 class CreateRelationships < ActiveRecord::Migration[6.1]
   def change
     create_table :relationships do |t|
-      t.integer :follower_id,   default: 0, null: false
-      t.integer :follow_id,     default: 0, null: false
+      t.integer :follower_id,   default: "", null: false
+      t.integer :follow_id,     default: "", null: false
       t.timestamps
     end
   end
