6cc92d89be1c0f312aae9b701264dc42240c7e6a
satos310 <119788681+satos310@users.noreply.github.com>
2023-06-25 04:37:28 +0000

    [Fix] Guest User Login

--- app/models/user.rb
+++ app/models/user.rb
@@ -51,6 +51,7 @@ class User < ApplicationRecord
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
       user.name = "ゲストユーザー"
+      user.introduction = "ゲストユーザーです。"
       # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
       # 例えば name を入力必須としているため，user.name = "ゲストユーザー"
     end
